e4b55ae014525567794d0c2aba3b1faf3409600c
Daniel <dlfp16@student.aau.dk>
2019-03-21 10:15:09 +0100

    corrected the unity folder

---
+++ Only_Unity_Python_Com/UnityProject/Assets/Scripts/HelloClient.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using AsyncIO;
+using NetMQ;
+using NetMQ.Sockets;
+using UnityEngine;
+
+public class HelloClient : MonoBehaviour
+{
+    private HelloRequester _helloRequester;
+    public bool SendPack = true;
+    public Quaternion joint;
+    public Transform pos;
+    string message;
+    public int  niceguy = 5;
+    void Update()
+    {
+        if (SendPack)
+        {
+            _helloRequester.messageToSend = niceguy.ToString();
+            
+            _helloRequester.Continue();
+        } else if (!SendPack)
+        {
+            _helloRequester.Pause();
+        }
+    }
+
+    private void Start()
+    {
+        message = "hahaha i have sent a message";
+        _helloRequester = new HelloRequester();
+        _helloRequester.Start();
+    }
+
+    private void OnDestroy()
+    {
+        _helloRequester.Stop();
+    }
+}
